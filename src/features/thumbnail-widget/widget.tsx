8a348d3c0cfbcd8a45a61517be86023e29d46da9
kh4f <kh4f.dev@gmail.com>
2026-02-25 20:01:11 +0300

    refactor(thumbnail-widget): consolidate styles into single gcss block
    Move link styles from separate `link` CSS variable into the main `gcss` global stylesheet to reduce component complexity.

--- src/features/thumbnail-widget/widget.tsx
+++ src/features/thumbnail-widget/widget.tsx
@@ -3,17 +3,10 @@ import { log } from '@/utils'
 export const ThumbnailWidget = ({ url }: { url: string }) => {
 	log('Thumbnail widget rendered with URL:', url)
 
-	return <a className={link} href={url} target="_blank" rel="noreferrer">
+	return <a href={url} target="_blank" rel="noreferrer">
 		<img src={url} alt="thumbnail"/>
 	</a>
 }
-
-const link = css`
-	display: flex;
-	justify-content: center;
-
-	img { width: 100%; }
-`
 
 void gcss`
 	.watch-root-element #secondary {
@@ -26,6 +19,13 @@ void gcss`
 			margin-bottom: 16px;
 			border-radius: 12px;
 			border: 1px solid hsla(0, 0%, 100%, 0.1);
+
+			a {
+				display: flex;
+				justify-content: center;
+
+				img { width: 100%; }
+			}
 		}
 	}
 `
