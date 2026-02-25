73e0ce9cb01b78bd0bdaf42a0a6ee7bb7d93cf47
kh4f <kh4f.dev@gmail.com>
2026-02-25 18:26:24 +0300

    feat(playlist-sorting): auto-refresh grid on playlist rename
    Monitor YouTube API requests for `browse/edit_playlist` operations. When detected, automatically refresh the grid renderer to reflect playlist name updates without requiring a page reload.

--- src/background.ts
+++ src/background.ts
@@ -2,3 +2,27 @@ chrome.tabs.onUpdated.addListener((tabId, onUpdatedInfo, tab) => {
 	if (onUpdatedInfo.status === 'complete')
 		void chrome.tabs.sendMessage(tabId, { action: 'tab-updated', tabInfo: tab }).catch(() => null)
 })
+
+chrome.webRequest.onCompleted.addListener(details => {
+	console.log('[THUMO|BG] Detected playlist changes, forcing YT grid refresh')
+
+	void chrome.scripting.executeScript({
+		target: { tabId: details.tabId },
+		world: 'MAIN',
+		func: () => {
+			type GridRenderer = Element & {
+				data: any // eslint-disable-line @typescript-eslint/no-explicit-any
+				resolveCommand: (cmd: unknown) => boolean
+				forceRefreshAndLog: () => void
+			}
+
+			const gridRenderer = document.querySelector<GridRenderer>('ytd-rich-grid-renderer:has(#thumo-playlist-grid)')!
+			console.log('[THUMO|WORLD] Found grid renderer:', gridRenderer)
+
+			const res = gridRenderer.resolveCommand(gridRenderer.data.header.chipBarViewModel.chips[0].chipViewModel.tapCommand.innertubeCommand.showSheetCommand.panelLoadingStrategy.inlineContent.sheetViewModel.content.listViewModel.listItems[0].listItemViewModel.rendererContext.commandContext.onTap.innertubeCommand) // eslint-disable-line
+			console.log('[THUMO|WORLD] Command execution result:', res)
+
+			gridRenderer.forceRefreshAndLog()
+		},
+	})
+}, { urls: ['https://www.youtube.com/youtubei/v1/browse/edit_playlist*'] })
